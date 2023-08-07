6aa9c58e7a5f3888dbcefb621f37d09301dd9fb0
Platon Orel <takezohensai@gmail.com>
2023-08-07 20:13:21 +0300

    добавлены настройки порта

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import serial.tools.list_ports
 ports = serial.tools.list_ports.comports()
 for port in ports:
     print('Доступные com порты: ', port.device)
+
+baudrate = 9600 #скорость порта
+port = str(input('Введите номер порта: '))
