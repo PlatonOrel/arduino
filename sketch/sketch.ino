6d51f1a2813f7a02c9a066760314288a997e5e5f
Platon Orel <takezohensai@gmail.com>
2023-08-07 20:49:34 +0300

    добавлен сервопривод

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,10 +1,13 @@
 #include "DHT.h"
+#include <Servo.h>
 #define DHTPIN 2 // Номер пина на плате
-
+Servo servo;
 DHT dht(DHTPIN, DHT11);
 void setup() {
   Serial.begin(9600);
   dht.begin();
+  servo.attach(9);   // класса Servo, серво присоединен к пину 9
+  servo.write(0);   // Выставляем начальное положение 
 }
 void loop() {
   delay(2000); // задержка
@@ -13,6 +16,7 @@ void loop() {
   if (isnan(h) || isnan(t)) {  // Проверка.
     Serial.println("Ошибка считывания");
     return;
+  
   }
   Serial.print("Влажность: ");
   Serial.print(h);
@@ -20,4 +24,16 @@ void loop() {
   Serial.print("Температура: ");
   Serial.print(t);
   Serial.println(" *C "); //Вывод показателей на экран
+  servo.write(360); // Поворачиваем серво на 90 градусов
+  delay(1000);        
+  servo.write(1800);
+  delay(100);        
+  servo.write(90);
+  delay(1000);
+  servo.write(0);
+  delay(1000);     
 }
+
+
+
+
